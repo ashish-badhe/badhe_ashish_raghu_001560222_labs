042cd3c5724e301b2490c2bda81475fb9e4f7920
ashish-badhe <badhe.a@northeastern.edu>
2023-09-14 18:17:14 -0400

    lab 1 Simple Swing UI

---
+++ Lab_1/src/model/Product.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author ashish
+ */
+public class Product {
+    
+    private String name;
+    private String description;
+    private String availNum;
+    private String price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAvailNum() {
+        return availNum;
+    }
+
+    public void setAvailNum(String availNum) {
+        this.availNum = availNum;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+    
+    
+    
+}
